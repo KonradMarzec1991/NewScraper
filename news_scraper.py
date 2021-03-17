d5ebcd83b6bc72cb4c73210e3c085e534c66a668
KonradMarzec1991 <konrimarzec@gmail.com>
2021-03-17 21:56:08 +0100

    Add func for polsat news

--- news_scraper.py
+++ news_scraper.py
@@ -9,7 +9,10 @@ def get_webpage_html(url):
 
 # for onet
 def get_onet():
-    soup = BeautifulSoup(get_webpage_html('https://www.onet.pl/'), "html.parser")
+    soup = BeautifulSoup(
+        get_webpage_html('https://www.onet.pl/'),
+        "html.parser"
+    )
     my_divs = soup.find_all('div', {'class': 'hpLiveColumn'})
 
     one = my_divs[0]
@@ -21,7 +24,10 @@ def get_onet():
 
 # for wp
 def get_wp():
-    soup = BeautifulSoup(get_webpage_html('https://www.wp.pl/'), "html.parser")
+    soup = BeautifulSoup(
+        get_webpage_html('https://www.wp.pl/'),
+        "html.parser"
+    )
     my_divs = soup.find_all('div', {'class', 'sc-1010b23-0 jmZodu'})
 
     one = my_divs[0]
@@ -33,7 +39,10 @@ def get_wp():
 
 # for interia
 def get_interia():
-    soup = BeautifulSoup(get_webpage_html('https://www.interia.pl/'), "html.parser")
+    soup = BeautifulSoup(
+        get_webpage_html('https://www.interia.pl/'),
+        "html.parser"
+    )
     my_divs = soup.find_all('section', {'id', 'special'})
 
     one = my_divs[0]
@@ -45,11 +54,21 @@ def get_interia():
 
 # for polsat news
 def get_polsat():
-    soup = BeautifulSoup(get_webpage_html('https://www.polsatnews.pl/'), "html.parser")
-    my_divs = soup.find_all('div', {'id', 'sg_slider'})
+    r = requests.get(
+        'https://www.polsatnews.pl',
+        headers={'User-Agent': 'Mozilla/5.0'}
+    )
+    soup = BeautifulSoup(
+        r.content,
+        "html.parser"
+    )
+    my_divs = soup.find_all('div', {'id': 'sg_slider'})
 
-    print(my_divs)
+    one = my_divs[0]
+    headers = one.find_all('img')
+
+    for h in headers:
+        print(h['alt'])
 
 
-get_wp()
 get_polsat()
