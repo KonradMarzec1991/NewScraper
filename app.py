47113d568e316604d6fa962740844e34e4e2cf56
KonradMarzec1991 <konrimarzec@gmail.com>
2021-03-24 22:02:53 +0100

    Add initial model

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # init app
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://new_user:news@localhost:5432/news'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class News(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    service = db.Column(db.String(50))
+    title = db.Column(db.String(150))
 
 
 @app.route('/')
