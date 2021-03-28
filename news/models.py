6d96c7dc024eb47ba04561cd0456653c79cdb395
KonradMarzec1991 <konrimarzec@gmail.com>
2021-03-28 15:01:52 +0200

    Add model, register in admin

--- news/models.py
+++ news/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    origin = models.CharField(max_length=50)
+    header = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now=True)
