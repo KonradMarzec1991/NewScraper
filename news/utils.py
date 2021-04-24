c4ccf644ac287373c9b533226cdf17d7d51befd6
KonradMarzec1991 <konrimarzec@gmail.com>
2021-04-24 18:45:17 +0200

    Add utils docstrings

--- news/utils.py
+++ news/utils.py
@@ -1,8 +1,13 @@
+"""
+This module contains helper function for scraper and/or model
+"""
+
 import requests
 from bs4 import BeautifulSoup
 
 
 def create_soup(service_name):
+    """This function creates bs4 object for given news service"""
     response = requests.get(
         url=f'https://www.{service_name}.pl',
         headers={'User-Agent': 'Mozilla/5.0'}
