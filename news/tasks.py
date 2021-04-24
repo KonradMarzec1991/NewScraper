9d383e2409230a401c2a6856396d8f201368c8be
KonradMarzec1991 <konrimarzec@gmail.com>
2021-04-24 18:43:04 +0200

    Add task for each service

--- news/tasks.py
+++ news/tasks.py
@@ -1,10 +1,34 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 
-from .scraper import Scraper
+from .scraper import (
+    wp,
+    onet,
+    interia,
+    polsatnews
+)
+
+
+def get_news(page):
+    page.get_board()
+    return f'{page.title()} news loaded successfully'
 
 
 @shared_task
-def get_news():
-    Scraper().get_news()
-    return 'Loaded successfully'
+def get_wp():
+    get_news(wp)
+
+
+@shared_task
+def get_interia():
+    get_news(interia)
+
+
+@shared_task
+def get_onet():
+    get_news(onet)
+
+
+@shared_task
+def get_polsatnews():
+    get_news(polsatnews)
