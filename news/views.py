769aa5d45e0b4e47983627bcc7f55966471db927
KonradMarzec1991 <konrimarzec@gmail.com>
2021-04-25 15:42:06 +0200

    Add post method for view

--- news/views.py
+++ news/views.py
@@ -1,6 +1,18 @@
+"""
+APIView module
+"""
+
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
 class RunNewsLoading(APIView):
-    pass
+    """This view delivers method for loading news data"""
+
+    def post(self, request):
+        """POST method for loading news"""
+        portal = request.POST.get('portal')
+
+        print(portal)
+        return Response('It works!')
