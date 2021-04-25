89b75231c093b1220e379386c5233e00714d7799
KonradMarzec1991 <konrimarzec@gmail.com>
2021-04-25 15:39:11 +0200

    Add DRF, create APIView

--- core/settings.py
+++ core/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_celery_beat'
+    'django_celery_beat',
+    'rest_framework'
 ]
 
 MY_APPS = [
