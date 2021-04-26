bbe95332b9fa1974a7324ba489094eb07c5010e0
KonradMarzec1991 <konrimarzec@gmail.com>
2021-04-26 18:28:25 +0200

    Call given func having prefix

--- news/views.py
+++ news/views.py
@@ -5,13 +5,8 @@ APIView module
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+import news.tasks as tasks
 from .serializers import PortalSerialier
-from .tasks import (
-    get_wp,
-    get_onet,
-    get_interia,
-    get_polsatnews
-)
 
 
 class RunNewsLoading(APIView):
@@ -21,15 +16,9 @@ class RunNewsLoading(APIView):
         """POST method for loading news"""
         ser = PortalSerialier(data=request.data)
         ser.is_valid(raise_exception=True)
+        portal = ser.data.get('portal')
 
-        portal = ser.data
-        if portal == 'onet':
-            get_onet()
-        if portal == 'wp':
-            get_wp()
-        if portal == 'interia':
-            get_interia()
-        if portal == 'polsatnews':
-            get_polsatnews()
+        method_to_call = getattr(tasks, f'get_{portal}')
+        method_to_call()
 
         return Response('Something works!')
